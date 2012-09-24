ef2485337eb642cd97aa1b8e6a004e2306027e5e
Daniel Plainview <n.konstantinov@co.wapstart.ru>
2012-09-24 16:25:51 +0400

    add geometric types (Point, Polygon) for Postgres

--- core/DB/PgSQL.class.php
+++ core/DB/PgSQL.class.php
@@ -210,6 +210,9 @@
 				
 				'ip4r'			=> DataType::IP_RANGE,
 				
+				'point'			=> DataType::POINT,
+				'polygon'		=> DataType::POLYGON,				
+				
 				// unhandled types, not ours anyway
 				'tsvector'		=> null,
 				
--- core/OSQL/DataType.class.php
+++ core/OSQL/DataType.class.php
@@ -40,6 +40,9 @@
 		const IP				= 0x000010;
 		const IP_RANGE			= 0x000011;
 		
+		const POINT				= 0x000012;
+		const POLYGON			= 0x000013;		
+		
 		const HAVE_SIZE			= 0x000100;
 		const HAVE_PRECISION	= 0x000200;
 		const HAVE_SCALE		= 0x000400;
@@ -77,7 +80,10 @@
 			self::BINARY		=> 'BINARY',
 			
 			self::IP			=> 'IP',
-			self::IP_RANGE		=> 'IP_RANGE'
+			self::IP_RANGE		=> 'IP_RANGE',
+			
+			self::POINT			=> 'POINT',
+			self::POLYGON		=> 'POLYGON'
 		);
 		
 		/**
