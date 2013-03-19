f3980fac3fbd9f4c784c1df4d1ec2308e99428b3
Evgeniy V. Kokovikhin <e.kokovikhin@co.wapstart.ru>
2013-03-19 19:17:08 +0400

    + PeclMemcache cas implemented

--- core/Cache/PeclMemcached.class.php
+++ core/Cache/PeclMemcached.class.php
@@ -23,6 +23,8 @@
 		const DEFAULT_HOST		= '127.0.0.1';
 		const DEFAULT_TIMEOUT	= 1;
 		
+		const CAS_EMPTY			= 'I_AM_EMPTY';
+		
 		protected $host			= null;
 		protected $port			= null;
 		private $instance		= null;
@@ -121,20 +123,31 @@
 		
 		public function get($index)
 		{
+			return $this->doGet($index);
+		}
+		
+		public function getc($index, &$cas)
+		{
+			return $this->doGet($index, $cas);
+		}
+		
+		public function cas($key, $value, $expires = Cache::EXPIRES_MEDIUM, $cas)
+		{
 			$this->ensureTriedToConnect();
 			
 			try {
-				return $this->instance->get($index);
+				return
+					$this->instance->cas(
+						$key, 
+						$value,
+						null,
+						$expires,
+						$cas
+					);
+				
 			} catch (BaseException $e) {
-				if(strpos($e->getMessage(), 'Invalid key') !== false)
-					return null;
-				
-				$this->alive = false;
-				
-				return null;
-			}
-			
-			Assert::isUnreachable();
+				return $this->alive = false;
+			}	
 		}
 		
 		public function delete($index)
@@ -237,8 +250,39 @@
 				$this->alive = true;
 				
 			} catch (BaseException $e) {
-				// bad luck.
+				// bad luck
 			}
+		}
+		
+		private function doGet($index, &$cas = self::CAS_EMPTY)
+		{
+			$this->ensureTriedToConnect();
+			
+			try {
+				$cazz = null;
+				
+				$result =
+					(
+						($cas === self::CAS_EMPTY)
+							? $this->instance->get($index)
+							: $this->instance->get($index, null, $cazz)
+					);
+				
+				$cas = $cazz;
+				
+				return $result;
+				
+			} catch (BaseException $e) {
+				throw $e;
+				if(strpos($e->getMessage(), 'Invalid key') !== false)
+					return null;
+				
+				$this->alive = false;
+				
+				return null;
+			}
+			
+			Assert::isUnreachable();
 		}
 	}
 ?>
