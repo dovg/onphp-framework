6793d2168ba5bbffd3cccaa536e7eba0a8aae37a
aeryaguzov <dev.aeryaguzov@gmail.com>
2013-04-26 11:54:01 +0000

    add tree rule

---
+++ main/Utils/Routers/RouterTreeRule.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: aeryaguzov
+ * Date: 26.04.13
+ * Time: 12:00
+ * To change this template use File | Settings | File Templates.
+ */
+
+class RouterTreeRule {
+
+}
